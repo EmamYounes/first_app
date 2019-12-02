5f7620e607abf1b73421a7a21a4baeb14e123cb1
Emam <emamAhmed36@gmail.com>
2019-12-02 12:35:01 +0200

    do a refactor to create product screen

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -22,32 +22,9 @@ class _ProductCreatePage extends State<ProductCreatePage> {
       margin: EdgeInsets.all(10.0),
       child: ListView(
         children: <Widget>[
-          TextField(
-            decoration: InputDecoration(labelText: 'Product Title'),
-            onChanged: (String title) {
-              setState(() {
-                _titleValue = title;
-              });
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Product Description'),
-            maxLines: 4,
-            onChanged: (String description) {
-              setState(() {
-                _descriptionValue = description;
-              });
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Product Price'),
-            keyboardType: TextInputType.number,
-            onChanged: (String price) {
-              setState(() {
-                _priceValue = double.parse(price);
-              });
-            },
-          ),
+          _buildTitleTextField(),
+          _buildDescriptionTextField(),
+          _buildPriceTextField(),
           SizedBox(
             height: 10.0,
           ),
@@ -56,18 +33,57 @@ class _ProductCreatePage extends State<ProductCreatePage> {
             textColor: Colors.white,
             child: Text('Save'),
             onPressed: () {
-              final Map<String, dynamic> products = {
-                'title': _titleValue,
-                'description': _descriptionValue,
-                'price': _priceValue,
-                'image': 'assets/food.jpg'
-              };
-              widget.addProduct(products);
-              Navigator.pushReplacementNamed(context, '/products');
+              _submitForm(context);
             },
           )
         ],
       ),
     );
+  }
+
+  Widget _buildTitleTextField() {
+    return TextField(
+      decoration: InputDecoration(labelText: 'Product Title'),
+      onChanged: (String title) {
+        setState(() {
+          _titleValue = title;
+        });
+      },
+    );
+  }
+
+  Widget _buildDescriptionTextField() {
+    return TextField(
+      decoration: InputDecoration(labelText: 'Product Description'),
+      maxLines: 4,
+      onChanged: (String description) {
+        setState(() {
+          _descriptionValue = description;
+        });
+      },
+    );
+  }
+
+  Widget _buildPriceTextField() {
+    return TextField(
+      decoration: InputDecoration(labelText: 'Product Price'),
+      keyboardType: TextInputType.number,
+      onChanged: (String price) {
+        setState(() {
+          _priceValue = double.parse(price);
+        });
+      },
+    );
+  }
+
+  void _submitForm(BuildContext context) {
+    final Map<String, dynamic> products = {
+      'title': _titleValue,
+      'description': _descriptionValue,
+      'price': _priceValue,
+      'image': 'assets/food.jpg'
+    };
+    widget.addProduct(products);
+    Navigator.pushReplacementNamed(context, '/products');
   }
 }
