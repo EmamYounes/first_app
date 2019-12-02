3e6b94658706cf987337e540a70751cdf49ebcf8
Emam <emamAhmed36@gmail.com>
2019-12-02 13:04:48 +0200

    do a refactor to create products and auth screen

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -6,13 +6,16 @@ import 'package:flutter/material.dart';
 class ProductPage extends StatelessWidget {
   final String title;
   final String imageUrl;
+  final double price;
+  final String description;
 
-  ProductPage(this.title, this.imageUrl);
+  ProductPage(this.title, this.imageUrl, this.price, this.description);
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () {
+        print('Back button pressed!');
         Navigator.pop(context, false);
         return Future.value(false);
       },
@@ -28,17 +31,44 @@ class ProductPage extends StatelessWidget {
               padding: EdgeInsets.all(10.0),
               child: TitleDefault(title),
             ),
+            _buildAddressPriceRow(),
             Container(
               padding: EdgeInsets.all(10.0),
               child: RaisedButton(
                 color: Colors.deepPurpleAccent,
-                child: Text('DELETE'),
+                child: Text(
+                  description,
+                  textAlign: TextAlign.center,
+                ),
                 onPressed: () => _showWarningDialog(context),
               ),
             )
           ],
         ),
       ),
+    );
+  }
+
+  Row _buildAddressPriceRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Union Square, San Francisco',
+          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 5.0),
+          child: Text(
+            '|',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        Text(
+          '\$' + price.toString(),
+          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+        ),
+      ],
     );
   }
 
