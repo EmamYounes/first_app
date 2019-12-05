ebd14186c864c4d098ee1e3ffd0343e9cb7db2c7
Emam <emamAhmed36@gmail.com>
2019-12-05 10:51:07 +0200

    add Dismissible widget to dismiss item from list and delete it from the list

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -4,14 +4,21 @@ import 'package:flutter/material.dart';
 class ProductListPage extends StatelessWidget {
   final List<Map<String, dynamic>> products;
   final Function updateProduct;
+  final Function deleteProduct;
 
-  ProductListPage(this.products, this.updateProduct);
+  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         itemBuilder: (BuildContext context, int index) {
           return Dismissible(
+            onDismissed: (DismissDirection dismissDirection) {
+              if (dismissDirection == DismissDirection.endToStart) {
+                deleteProduct(index);
+              } else if (dismissDirection == DismissDirection.startToEnd) {
+              } else {}
+            },
             background: Container(
               color: Colors.red,
             ),
