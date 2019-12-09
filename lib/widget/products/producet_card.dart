f4ed5746b60021586f267d3025d39dc09f6e72b2
Emam <emamAhmed36@gmail.com>
2019-12-09 14:45:58 +0200

    add favorite  logic

--- lib/widget/products/producet_card.dart
+++ lib/widget/products/producet_card.dart
@@ -1,8 +1,10 @@
 import 'package:first_app/models/product.dart';
+import 'package:first_app/scoped-models/products.dart';
 import 'package:first_app/widget/products/address_tag.dart';
 import 'package:first_app/widget/products/price_tag.dart';
 import 'package:first_app/widget/ui_elements/title_default.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
@@ -24,39 +26,45 @@ class ProductCard extends StatelessWidget {
     );
   }
 
-  ButtonBar _buildActionButtons(BuildContext context) {
+  Widget _buildActionButtons(BuildContext context) {
     return ButtonBar(
-          alignment: MainAxisAlignment.center,
-          children: <Widget>[
-            IconButton(
-              color: Theme.of(context).accentColor,
-              icon: Icon(Icons.info),
-              onPressed: () => Navigator.pushNamed<bool>(
-                  context, '/product/' + productIndex.toString()),
-            ),
-            IconButton(
-              icon: Icon(Icons.favorite_border),
+      alignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          color: Theme.of(context).accentColor,
+          icon: Icon(Icons.info),
+          onPressed: () => Navigator.pushNamed<bool>(
+              context, '/product/' + productIndex.toString()),
+        ),
+        ScopedModelDescendant<ProductsModel>(
+            builder: (BuildContext context, Widget child, ProductsModel model) {
+          return IconButton(
+              icon: Icon(model.products[productIndex].isFavorite
+                  ? Icons.favorite
+                  : Icons.favorite_border),
               color: Colors.red,
-              onPressed: () => Navigator.pushNamed<bool>(
-                  context, '/product/' + productIndex.toString()),
-            )
-          ],
-        );
+              onPressed: () {
+                model.selectProduct(productIndex);
+                model.toggleProductFavoriteStatus();
+              });
+        }),
+      ],
+    );
   }
 
   Container _buildTitlePriceContainer() {
     return Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TitleDefault(product.title),
-              SizedBox(
-                width: 8.0,
-              ),
-              Pricetag(product.price.toString())
-            ],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TitleDefault(product.title),
+          SizedBox(
+            width: 8.0,
           ),
-          margin: EdgeInsets.only(top: 10.0),
-        );
+          Pricetag(product.price.toString())
+        ],
+      ),
+      margin: EdgeInsets.only(top: 10.0),
+    );
   }
 }
