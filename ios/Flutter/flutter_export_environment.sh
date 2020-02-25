6289b9495eac0d309fcf940149397532d77995c2
Emam <emamAhmed36@gmail.com>
2020-02-25 09:30:08 +0200

    fix build to run

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\FlutterSDK\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\FlutterApp\first_app"
+export "FLUTTER_ROOT=D:\FlutterSDK\FlutterSdk_1.9.1\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\NewProject\FirstApp"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=D:\FlutterSDK\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\FlutterSDK\FlutterSdk_1.9.1\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
