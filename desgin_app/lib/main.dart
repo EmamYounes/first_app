f9d9a38e8ff1e1260ebcfd1031399d19239bee2d
Emam Ahmed <emamAhmed36@gmail.com>
2020-05-06 15:09:07 +0200

    init

---
+++ desgin_app/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: buildBody(context),
+    );
+  }
+
+  Widget buildBody(BuildContext context) {
+    return Center(
+      child: Stack(
+        alignment: Alignment(1.3, -.1),
+        children: <Widget>[
+          Icon(
+            Icons.call_to_action,
+            color: Colors.red,
+            size: 100,
+          ),
+          Icon(
+            Icons.access_time,
+            color: Colors.blue,
+            size: 40,
+          ),
+        ],
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      title: Text(widget.title),
+    );
+  }
+}
